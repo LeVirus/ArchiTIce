775fb88eadcba3c246065331cb502adc7d2c52f1
LeVirus <charbonneau_cyril@hotmail.fr>
2016-03-01 15:31:54 +0100

    Correction bug monitor

--- include/jBiblAudio.h
+++ include/jBiblAudio.h
@@ -10,38 +10,47 @@
 #include "../include/IceUtil/Monitor.h"
 
 
+
+
 struct Morceau
 {
-    std::string msNomArtiste, msFichier;
-    int muiDureeMorceau, muiDateSortie;
-    MonitorPrx monitor;
+	std::string msNomArtiste, msFichier;
+	int muiDureeMorceau, muiDateSortie;
+};
+
+class MonitorI : virtual public biblAudio::Monitor{
+	public:
+		virtual void report(const std::string& action, const Morceau &morceau, const Ice::Current&) {
+			std::cout<<action<<"\n";
+		}
 };
 
 class BiblAudio : public  biblAudio :: ServeurIce
 {
-    private:
-	biblAudio::mvectRecherche mvectRecherche;
-	biblAudio::mmapMorceaux  mmapMorceaux;
-	libvlc_instance_t * instVLC;
-	libvlc_media_player_t *mediaPlayerVLC;
-	libvlc_media_t *mediaVLC;
-	std::string pathToLemmy;
-    public:
-	BiblAudio();
-	biblAudio::mvectRecherche getMorceauxArt( const std::string &sNomArtiste, const Ice::Current& );
-	biblAudio::Morceau rechercherMorceauVect( biblAudio::mvectRecherche mvectMorc,const std::string &nomMorc ); 
-	biblAudio::mvectRecherche getMorceauxMorc( const std::string &sNomMorc, const Ice::Current& );
-	void afficherMorceaux(const Ice::Current&);
-	void stopSound(const Ice::Current&);
+	private:
+		biblAudio::mvectRecherche mvectRecherche;
+		biblAudio::mmapMorceaux  mmapMorceaux;
+		biblAudio::MonitorPrx monitor;
+		libvlc_instance_t * instVLC;
+		libvlc_media_player_t *mediaPlayerVLC;
+		libvlc_media_t *mediaVLC;
+		std::string pathToLemmy;
+	public:
+		BiblAudio();
+		biblAudio::mvectRecherche getMorceauxArt( const std::string &sNomArtiste, const Ice::Current& );
+		biblAudio::Morceau rechercherMorceauVect( biblAudio::mvectRecherche mvectMorc,const std::string &nomMorc ); 
+		biblAudio::mvectRecherche getMorceauxMorc( const std::string &sNomMorc, const Ice::Current& );
+		void afficherMorceaux(const Ice::Current&);
+		void stopSound(const Ice::Current&);
 
-	biblAudio::Morceau bRechercherMorceau(const std::string &sNomMusique, const Ice::Current&);
+		biblAudio::Morceau bRechercherMorceau(const std::string &sNomMusique, const Ice::Current&);
 
-	bool bAjoutMorceau(const std::string &sNomArt, const std::string &sNomMorc,const  std::string &sFic, int uiDureeMorc , int uiDateSortie, const Ice::Current&);
-	bool bSuprMorceau(const std::string &sNomArt,const std::string &sNomMorc, const Ice::Current&);
-	void readSoundFic(const std::string &pathToFic,const Ice::Current&);
-	void readSound(const std::string &sNomMorceau,const std::string &sNomArtiste,const Ice::Current&);
-	void prepareSong();
-	~BiblAudio();
+		bool bAjoutMorceau(const std::string &sNomArt, const std::string &sNomMorc,const  std::string &sFic, int uiDureeMorc , int uiDateSortie, const Ice::Current&);
+		bool bSuprMorceau(const std::string &sNomArt,const std::string &sNomMorc, const Ice::Current&);
+		void readSoundFic(const std::string &pathToFic,const Ice::Current&);
+		void readSound(const std::string &sNomMorceau,const std::string &sNomArtiste,const Ice::Current&);
+		void prepareSong();
+		~BiblAudio();
 };
 
 #endif // BIBLAUDIO_H 
